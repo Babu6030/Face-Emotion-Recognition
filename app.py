e43eb3e8c8ea747c3ac8674dd8116c27d01817b4
Babu Reddy <babureddy6030@gmail.com>
2021-07-25 22:59:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,87 +1,49 @@
-import os
 import numpy as np
+from PIL import Image, ImageOps
+import cv2
 import streamlit as st
-import random
-import importlib  
-from webcam import webcam
-from PIL import Image 
-import tensorflow.compat.v1 as tf
-import matplotlib.pyplot as plt
-import utils as ut
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import img_to_array, array_to_img
 
-import footer
+st.set_option('deprecation.showfileUploaderEncoding', False)
 
-from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
+#model = model_from_json(open("vgg16_model.json", "r").read())
+model=load_model('https://github.com/Babu6030/Face-Emotion-Recognition/blob/main/cnn_model.h5')
+model.load_weights('https://github.com/Babu6030/Face-Emotion-Recognition/blob/main/cnn_model.h5')
 
-# matplotlib params
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-# old tensorflow
-tf.disable_eager_execution()
-
-nr_max_faces=20
-nc=6
-
-# load my V1 tensorflow graph as default 
-y, xin, keep_prob_input = ut.set_tf_model_graph(nr_max_faces)
-sess = tf.Session()
-saver = tf.train.Saver()
-
-# restore the weights from file
-saver.restore(sess, 'https://github.com/Babu6030/Face-Emotion-Recognition/blob/main/Kaggle%20Notebooks/OpenCv%20Code/modelbestweights.h5')
-
-### Streamlit app
-st.title("Facial Emotion Recognizer")
-st.subheader("&#8592; Choose the image source on the sidebar:")
-st.markdown("test image, webcam, or upload")
-st.markdown("Frontal face images without glasses work best. Image is not stored or saved in any form.")
-st.markdown("Dislaimer: Use this app at your own risk. Result might be mind-boggling.")
-st.subheader('''First, OpenCV will detect faces, (based on [this](https://realpython.com/face-recognition-with-python/)).''')
-st.subheader('''Then, Tensorflow will recognize their emotions using [my custom neural net](https://github.com/Babu6030/Face-Emotion-Recognition).''')
-
-source = st.sidebar.selectbox( "Image Source? ", ('Test Image', 'Upload', 'Webcam') )
-
-if source == 'Webcam':
-    captured_image = webcam()
-elif source == 'Upload':
-    file = st.file_uploader("Upload your img file here (.jpg or .png)")
-    if file is not None:
-        captured_image = Image.open(file)
-    else :
-        captured_image = None
-elif source == 'Test Image':
-    captured_image = Image.open("test-images/the-beatles.png")
+st.write("""# Face Expression Recognition""")
+st.write("This Model predicts the expression on a persons face.")
+file = st.file_uploader("Please upload an image file", type=["jpg"])
 
 
-if captured_image is None:
-    st.write("Waiting for image...")
+def import_and_predict(image_data, model):
+    size = (150, 150)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = np.asarray(image)
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    face_haar_cascade = cv2.CascadeClassifier(
+        'haarcascade_frontalface_default.xml')
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    for (x, y, w, h) in faces_detected:
+        roi_gray = gray_img[y:y+w, x:x+h]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        img_pixels = img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255
+        predictions = model.predict(img_pixels)
+        max_index = np.argmax(predictions[0])
+        emotions = ('ANGRY 😡', 'HAPPY 😀', 'NEUTRAL 😐', 'SAD 🙁')
+        predicted = emotions[max_index]
+    return predicted
+
+
+if file is None:
+    st.text("Please upload an image file")
 else:
-    st.write("Got an image from the {}:".format(source.lower()))
-
-    faces, marked_img = ut.get_faces_from_img(np.array(captured_image))
-    st.image(marked_img, use_column_width=True)
-
-    st.subheader('Found {} faces in the picture above:'.format(len(faces)) )
-
-    if len(faces):      
-        #creating the blank test vector, must be length of batch_size (old v1 tensorflow)
-        data_orig = np.zeros([nr_max_faces, 48,48])
-
-        nr_faces = min(len(faces), 20)
-
-        #putting face data into the vector (only first few), max 20 faces
-        for i in range(0, nr_faces):
-            data_orig[i,:,:] = ut.contrast_stretch(faces[i,:,:])
-
-        #preparing images
-        data = ut.preprocess_faces(data_orig)
-
-        # run the DNN
-        result = sess.run([y], feed_dict={xin: data, keep_prob_input: 1.0})
-        
-        for i in range(0, nr_faces):
-            with _lock: # https://matplotlib.org/3.3.2/faq/howto_faq.html#working-with-threads
-                plt = ut.plot_face(result[0][i], data[i,:])
-                st.pyplot(plt)
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    st.text("The given image has the following expression:")
+    st.write(prediction)
