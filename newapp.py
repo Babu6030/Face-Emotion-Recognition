970e8e3a7eeca4664c5ca2734affafb1369fc4d7
Babu Reddy <babureddy6030@gmail.com>
2021-07-27 23:51:06 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 face_classifier=cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
 
-classifier=load_model(r'modelbestweights.h5')
+classifier=load_model(r'cnn_model.h5')
 
 emotion_labels=['Angry','Disgust','Fear','Happy','Neutral','Sad','Surprise']
 
