f4e79b566d5ff147fdf818c20d922209358cf8d6
Babu Reddy <babureddy6030@gmail.com>
2021-08-04 00:44:52 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -63,6 +63,7 @@ while True:
     frame= cam.read()
     
     labels = []
+ 
 
 
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -91,4 +92,25 @@ while True:
     restore.image(frame)
   
   
+  
+  if __name__ == "__main__":
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
+    main()
   
