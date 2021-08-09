94bb0cdad126dddf64d505a1473ee350d058efc4
Babu Reddy <babureddy6030@gmail.com>
2021-08-09 21:52:39 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -66,9 +66,7 @@ while True:
     
 
 
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    cv2.imshow('Gray image', gray)  
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
 
     
     faces=face_classifier.detectMultiScale(gray)
